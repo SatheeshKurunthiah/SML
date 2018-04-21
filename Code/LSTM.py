8b5fe2e7d84d9cfb27b20c3faf2444ca89f6246c
Midhun PM <midhunpm101@gmail.com>
2018-04-21 10:26:42 -0700

    LSTM tensorflow

--- Code/LSTM.py
+++ Code/LSTM.py
@@ -1,13 +1,17 @@
 import tensorflow as tf
 import numpy as np
 import tensorflow.contrib.layers as layers
+import Formatter
+
+period_sample = Formatter.PeriodSample(10)
+
 
 TINY = 1e-6 # to avoid NaNs in logs
-INPUT_SIZE =20 
-OUTPUT_SIZE = 20
+INPUT_SIZE =4 
+OUTPUT_SIZE = 1
 LSTM_CELL_UNITS = 20
 LEARNING_RATE = 0.01
-inputs = tf.placeholder(tf.float32, shape = (None, None, INPUT_SIZE)) #(time, batch, in)
+inputs = tf.placeholder(tf.float32, shape = (None, None, INPUT_SIZE*4)) #(time, batch, in)
 outputs = tf.placeholder(tf.float32, shape = (None, None, OUTPUT_SIZE))
 
 
@@ -17,11 +21,14 @@ batch_size = tf.shape(inputs)[1]
 initial_state = cell.zero_state(batch_size, tf.float32)
 rnn_outputs, rnn_states = tf.nn.dynamic_rnn(cell, inputs, initial_state=initial_state, time_major=True)
 final_projection = lambda x: layers.linear(x, num_outputs=OUTPUT_SIZE, activation_fn=tf.nn.sigmoid)
+rnn_outputs = tf.Print(rnn_outputs,[rnn_outputs],"rnn_outputs: ")
 predicted_outputs = tf.map_fn(final_projection, rnn_outputs)
-
+predicted_outputs = tf.Print(predicted_outputs,[predicted_outputs],"predicted_outputs : ")
 # compute elementwise cross entropy.
 error = -(outputs * tf.log(predicted_outputs + TINY) + (1.0 - outputs) * tf.log(1.0 - predicted_outputs + TINY))
 error = tf.reduce_mean(error)
+
+
 
 # optimize
 train_fn = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE).minimize(error)
@@ -33,10 +40,11 @@ accuracy = tf.reduce_mean(tf.cast(tf.abs(outputs - predicted_outputs) < 0.5, tf.
 
 
 def generate_batch(num_bits,batch_size):
-	x = np.random.random_integers(0,10,20)
-	y = np.concatenate((np.zeros(10),np.ones(10)),axis=0)
-	x=x.reshape(1,1,20)
-	y=y.reshape(1,1,20)
+	out = period_sample.getIndicatorData(INPUT_SIZE,3)
+	x=out[0]
+	y=np.asarray(out[1])
+	x=x.reshape(1,1,INPUT_SIZE*4)
+	y=y.reshape(1,1,OUTPUT_SIZE)
 	return x,y
 
 
@@ -58,6 +66,9 @@ for epoch in range(1000):
         # here train_fn is what triggers backprop. error and accuracy on their
         # own do not trigger the backprop.
         x, y = generate_batch(num_bits=NUM_BITS, batch_size=BATCH_SIZE)
+        # print (x)
+        # print ("--")
+        # print (y)
         epoch_error += session.run([error, train_fn], {
             inputs: x,
             outputs: y,
